86e040aa9e59373e159a9e51f5e70bd254d972bb
Sabrina-Lopez <sa568892@ucf.edu>
2024-02-21 14:37:16 -0500

    modified subset of open images v7 dataset to download and fine-tune with + fine-tune program change to ultralytics tune library package

--- train_and_val.py
+++ train_and_val.py
@@ -6,6 +6,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 # model = YOLO("yolov8n.yaml")  # Build a new model from scratch
 model = YOLO("yolov8n.pt")  # Load a pretrained model (recommended for training)
 
+"""
 # Train and validate the model
 model.train(data="config.yaml", epochs=1)
 metrics = model.val()  # evaluate model performance on the validation set
@@ -15,3 +16,6 @@ metrics.box.maps   # a list contains map50-95 of each category
 
 # Save the model
 model.export(format="saved_model")  # export the model to ONNX format
+"""
+
+model.tune(data='config.yaml', epochs=1, iterations=10, optimizer='AdamW', plots=False, save=True, val=True)
